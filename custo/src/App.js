e639c5c7c92822832b71e374429bef3ff0500817
Kingsley <kingsleyumujeyan@gmail.com>
2020-06-27 23:00:34 +0100

    Header added

--- custo/src/App.js
+++ custo/src/App.js
@@ -1,27 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{ Component } from 'react';
+import { BrowserRouter as Router,Route,Switch } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Header from './components/views/header';
 
-export default App;
+
+
+export default class App extends Component {
+  render(){
+    return (
+      <Router>
+         <Header/>
+        <Container>
+            <Switch>
+              <Route/>
+            </Switch>
+        </Container>
+      </Router>
+    );
+  }
+  }
+  
